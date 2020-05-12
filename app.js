fdc1356fec467b848755b1dcaeebf02de149322e
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-12 13:44:07 -0500

    made more js adjustments

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ let friends = ["Don", "Sherm", "Sean", "Ricky", "Chris"];
 for (let i = 0; i < friends.length; i++) {
     console.log(friends[i]);
 
-    function song() {
-        for (let c = 99; c >= 1; c--) {
+    for (let c = 99; c >= 1; c--) {
 
-            if (c == 1) {
-                console.log(c + " line of code in the file, " + c + " line of code; " + friends[i] + " strikes out, clears it all out, no more lines of code in the file");
-            }
-            else {
-                console.log(c + " lines of code in the file, " + c + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (c - 1) + " lines of code in the file");
-            }
+        if (c == 1) {
+            console.log(c + " line of code in the file, " + c + " line of code; " + friends[i] + " strikes out, clears it all out, no more lines of code in the file");
+        }
+        else {
+            console.log(c + " lines of code in the file, " + c + " lines of code; " + friends[i] + " strikes out, clears it all out, " + (c - 1) + " lines of code in the file");
         }
     }
-    console.log(song());
 }
